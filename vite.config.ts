30985289652de77d35638e34c7d10919b25a8ebc
RingoTangs <1466637477@qq.com>
2024-01-28 18:23:39 +0800

    feat: vite.config.ts add legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,22 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import legacy from '@vitejs/plugin-legacy'
 import tailwindcss from 'tailwindcss'
 import postcssPresetEnv from 'postcss-preset-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    // Terser must be installed because plugin-legacy uses Terser for minification.
+    legacy({
+      // polyfills config core.js and regenerator-runtime
+      // https://github.com/vitejs/vite/blob/main/packages/plugin-legacy/src/index.ts#L169
+      polyfills: ['stable/index', 'regenerator'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
@@ -16,6 +26,11 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [postcssPresetEnv(), tailwindcss()],
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['vue'],
     },
   },
 })
